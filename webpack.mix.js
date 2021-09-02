6ba7ef4d46eb5e9cda611df7b67092b74beeb842
Quade du Toit <quadekenneth@gmail.com>
2021-09-02 05:16:10 +0200

    added custom framework, icons and fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,7 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.copyDirectory('resources/fonts', 'public/assets/fonts');
+mix.copyDirectory('resources/icons', 'public/assets/icons');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/app.js', 'public/assets/js').vue()
+    .sass("resources/sass/app.scss", "public/assets/css");
